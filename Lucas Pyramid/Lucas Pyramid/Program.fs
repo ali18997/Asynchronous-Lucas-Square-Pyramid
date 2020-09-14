2afe9e898b8a079f95f157b6c42382f565e2d28f
Ali <mohammadali18997@gmail.com>
2020-09-14 18:32:07 -0400

    timing added

--- Lucas Pyramid/Lucas Pyramid/Program.fs
+++ Lucas Pyramid/Lucas Pyramid/Program.fs
@@ -3,6 +3,20 @@ open Akka.FSharp
 open Akka.Configuration
 open System
 
+open System.Diagnostics
+
+let time f = 
+    let proc = Process.GetCurrentProcess()
+    let cpu_time_stamp = proc.TotalProcessorTime
+    let timer = new Stopwatch()
+    timer.Start()
+    try
+        f()
+    finally
+        let cpu_time = (proc.TotalProcessorTime-cpu_time_stamp).TotalMilliseconds
+        printfn "CPU time = %dms" (int64 cpu_time)
+        printfn "Absolute time = %dms" timer.ElapsedMilliseconds
+
 
 let check start length = 
     let mutable sum = 0
@@ -25,6 +39,7 @@ let lucasPyramid endPoint length =
                 flag <- false
 
     retVal
+    
 
-let a = lucasPyramid 100000000 4
-printfn "%i"a
+time (fun () -> lucasPyramid 1000000 4)
+
