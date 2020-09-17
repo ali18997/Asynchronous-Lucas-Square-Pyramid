4ec9928ebc75516cfdbde9f9622f001fca90a366
Ali <mohammadali18997@gmail.com>
2020-09-17 02:14:52 -0400

    Actor Partial Implementation

--- Lucas Pyramid/Lucas Pyramid/Program.fs
+++ Lucas Pyramid/Lucas Pyramid/Program.fs
@@ -2,6 +2,7 @@
 open Akka.FSharp
 open Akka.Configuration
 open System
+open Akka.Actor
 
 open System.Diagnostics
 
@@ -41,5 +42,91 @@ let lucasPyramid endPoint length =
     retVal
     
 
-time (fun () -> lucasPyramid 1000000 4)
+//time (fun () -> lucasPyramid 1000000 4)
 
+
+
+type ParentMessage() = 
+    [<DefaultValue>] val mutable endPoint: int
+    [<DefaultValue>] val mutable length: int
+    [<DefaultValue>] val mutable startFlag: bool
+    [<DefaultValue>] val mutable replyFlag: bool
+    [<DefaultValue>] val mutable replyVal: bool
+  
+
+type ChildMessage() = 
+    [<DefaultValue>] val mutable start: int
+    [<DefaultValue>] val mutable length: int
+    [<DefaultValue>] val mutable endPoint: int
+
+let system = System.create "system" <| Configuration.defaultConfig()
+
+let child (childMailbox:Actor<ChildMessage>) = 
+    let rec childLoop() = actor {
+        let! msg = childMailbox.Receive()
+
+        let mutable sum = 0
+        let start = msg.start
+        let length = msg.length
+        let mutable retVal = false
+
+        for i = start to start+length-1 do
+                sum <- sum + i*i
+        
+        if sqrt (float sum) % float 1 = 0.0 then
+            retVal <- true
+
+
+        let sender = childMailbox.Sender()
+        let reply = new ParentMessage()
+
+        reply.replyFlag <- true
+        reply.startFlag <- false
+        reply.endPoint <- start
+        reply.length <- length
+        reply.replyVal <- retVal
+
+        sender <! reply
+
+        return! childLoop()
+    }
+    childLoop()
+
+let parent (parentMailbox:Actor<ParentMessage>) =
+    // parent actor spawns child - is the supervisor of the child
+    let mutable workSize = 10000
+    let rec parentLoop() = actor {
+        let! (msg: ParentMessage) = parentMailbox.Receive()
+        if(msg.startFlag) then
+            for i = 1 to msg.endPoint do
+                printfn "%i" i
+                let name:string = "child" + i.ToString()
+                let child = spawn parentMailbox name child
+                let sendMessage = new ChildMessage()
+                sendMessage.start <- i
+                sendMessage.length <- msg.length
+                sendMessage.endPoint <- msg.endPoint
+                child <! sendMessage
+        
+        if(msg.replyFlag) then
+            printfn "%i" msg.endPoint
+            printfn "%b" msg.replyVal
+
+        return! parentLoop()
+    }
+    parentLoop()
+                        
+
+let mainMessage = new ParentMessage()
+mainMessage.endPoint <- 1000000
+mainMessage.length <- 4
+mainMessage.startFlag <- true
+mainMessage.replyFlag <- false
+mainMessage.replyVal <- false
+
+let parentActor = spawn system "parent" parent
+//parentActor <! mainMessage
+
+time (fun () -> parentActor <! mainMessage)
+
+system.Terminate()
